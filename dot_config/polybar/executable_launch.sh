d94138953c622b322536525f9382fe8fe8173fa6
Martin Pittermann <martinp2250@gmail.com>
2019-09-21 12:42:11 +0200

    i3 pc

--- dot_config/polybar/executable_launch.sh
+++ dot_config/polybar/executable_launch.sh
@@ -6,5 +6,5 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch Polybar, using default config location ~/.config/polybar/config
-polybar edp1 &
+polybar primary &
 echo "Polybar launched..."
--- run_once_install-packages.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-sudo pacman -S --needed fzf htop tmux nmon wget bash-completion fd
