0ab973b03d85f098db540eddad78e025c4955fd7
Martin Pittermann <martinp2250@gmail.com>
2019-09-22 09:40:03 +0200

    remove tray from secondary polybar

--- dot_config/compton/executable_launch.sh
+++ dot_config/compton/executable_launch.sh
@@ -2,6 +2,6 @@
 
 killall -q compton
 
-while pgrep -u $UID -x compton >/dev/null; do sleep 1; done
+while pgrep -u $UID -x compton >/dev/null; do sleep 0.2; done
 
 compton --config ~/.config/compton/compton.conf -b &
